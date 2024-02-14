9dfe00f89c509822143d47af1deca945efe1a7c8
Paulo Ricardo Ferreira <paulo.ricardo@agroterenas.com.br>
2024-02-14 20:02:21 -0300

    Create definicoes.sh

---
+++ definicoes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "================================="
+echo "==== Iniciando Configurações ===="
+echo "================================="
+
+echo  "\n"
+
+echo "==== Criandas pastas para o Airflow ===="
+
+mkdir -p ./airflow/dags ./airflow/logs ./airflow/plugins ./airflow/config
+
+echo  "\n"
+
+echo "==== Criando Arquivo .env Airflow ===="
+
+echo "AIRFLOW_UID=$(id -u)" > ./airflow/.env
+
+echo  "\n"
+
+echo "==== Criandas pastas para o Hop ===="
+
+mkdir projetos
